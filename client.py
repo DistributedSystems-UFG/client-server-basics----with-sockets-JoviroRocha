eaa44413aecea50ae122c2c011193c19219e7212
Joviro Rocha <joviro_neto@hotmail.com>
2022-11-01 20:46:10 -0300

    Second changes

--- client.py
+++ client.py
@@ -2,13 +2,28 @@ from asyncio import constants
 from socket  import *
 from constCS import * 
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-controler = "start"
-while True:
+def initialize(controler, s):
+    controler = "start"
     s.send(str.encode(controler))
+    recieveData(s)
+
+def sendData(s):
+    controler = input()
+    s.send(str.encode(controler))
+    return controler
+
+def recieveData(s):
     data = s.recv(1024)
     print (bytes.decode(data))
-    controler = input()
-    if (controler == "end"): break
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect((HOST, PORT)) # connect to server (block until accepted)
+controler = ""
+initialize(controler, s)
+
+while True:
+    
+    if (sendData(s) == "end"): break
+    recieveData(s)
+    
 s.close()
--- server.py
+++ server.py
@@ -8,12 +8,27 @@ s.listen(1)           #-
 while True:                # forever
   data = conn.recv(1024)   # receive data from client
   data = bytes.decode(data)
+  
   if not data: break       # stop if client stopped
-  if (data == "end"): 
+  
+  elif (data == "end"): 
     data = "\n \nOookay! \nSee you next time! \nHave an excelent day! \n \n"
     conn.send(str.encode(data))
     break
+  
   elif (data == "start"):
     data = "\n \nHello! \n \nWelcome to Distributed Computing System. \nType \"calculator\" to use the calculator. \nType \"end\" to end the program.\n\n"
     conn.send(str.encode(data)) # return sent data plus an "*"
+  
+  elif (data == "calculator"):
+    data = "\nGot it! Now choose one of the following operations and then choose the numbers that you want to apply the operation. \n1 - Addition. \n2 - Subtraction.\nAs the following example: (100 + 50) => 1 100 50.\n" 
+    conn.send(str.encode(data))
+    data = conn.recv(1024)   
+    data = bytes.decode(data)
+    print(data[0],data[1],data[2])
+
+
+  else:
+    data = "Oops, I could not understando your text. \nPlease try again.\n"
+    conn.send(str.encode(data))
 conn.close()               # close the connection
